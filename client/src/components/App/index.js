23579a50e516beb536d68014f21f3c0cec9bb9b7
JSLinn727 <justinslinn727@gmail.com>
2017-08-26 12:28:26 -0400

    Nav bar ya'll

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import './index.css'
+import Layout from '../Layout/index'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h2>Welcome to React</h2>
+        <Layout/>
       </div>
     )
   }
--- client/src/components/Layout/index.js
+++ client/src/components/Layout/index.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
 import './index.css'
+import logo from '../../assets/salt-block-catering-white.png'
 
 class Layout extends Component {
   render() {
     return (
       <div className="wrapper">
-        <h2>Welcome to React</h2>
+        <header>
+          <img className="logo" src={logo} alt="Salt Block Logo"/>
+          <nav>
+            <a href="">About</a>
+            <a href="">Hospitality</a>
+            <a href="">Contact</a>
+          </nav>
+        </header>
+        <footer></footer>
       </div>
     )
   }
