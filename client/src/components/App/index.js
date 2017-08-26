ae6ba3b2b0796c71cfa2d39e9895c72ba6839ec5
Adam Recvlohe <adam.recvlohe@gmail.com>
2017-08-26 17:59:43 -0400

    Add routing (#6)

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -3,14 +3,17 @@ import "./index.css";
 import Layout from "../Layout/index";
 import Services from "../Services/index";
 import PageOne from "../Packages/index";
+import { Switch, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Layout>
-          <PageOne />
-          <Services />
+          <Switch>
+            <Route exact path="/" component={PageOne} />
+            <Route path="/services" component={Services} />
+          </Switch>
         </Layout>
       </div>
     );
