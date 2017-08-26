5a8ca9e272ddb1a528156eb763a99e71faaa21e7
Adam Recvlohe <adam.recvlohe@gmail.com>
2017-08-26 17:42:47 -0400

    Fix error

--- client/src/components/Packages/index.js
+++ client/src/components/Packages/index.js
@@ -1,24 +1,24 @@
-import React, { Component } from 'react'
-import './index.css'
-import PageOne from './PageOne.js'
-import PageThree from './PageThree.js'
-import ProgressBar from './ProgressBar/ProgressBar'
+import React, { Component } from "react";
+import "./index.css";
+import PageOne from "./PageOne.js";
+import PageThree from "./PageThree.js";
+import ProgressBar from "./ProgressBar/ProgressBar";
 
 class Packages extends Component {
   render() {
-    let pageOneStyle = {}
-    let formStyle = {}
-    let inputStyle = {}
-    let labelStyle = {}
-    let textAreaStyle = {}
+    let pageOneStyle = {};
+    let formStyle = {};
+    let inputStyle = {};
+    let labelStyle = {};
+    let textAreaStyle = {};
     return (
       <div className="wrapper">
         <ProgressBar />
         <PageOne />
         <PageThree />
       </div>
-    )
+    );
   }
 }
 
-export default Packages
+export default Packages;
