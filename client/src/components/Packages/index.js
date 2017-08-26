d361950f4828af95dcdec4003455cdce3950f860
JSLinn727 <justinslinn727@gmail.com>
2017-08-26 10:56:22 -0400

    initial commit

---
+++ client/src/components/Packages/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './index.css'
+
+class Packages extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <h2>Welcome to React</h2>
+      </div>
+    )
+  }
+}
+
+export default Packages
