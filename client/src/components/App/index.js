f5877e24896889c942f5f424a893e190e01fdfd8
Cody Clark <c.susie.run@gmail.com>
2017-08-26 15:17:37 -0400

    Built out Page One. Added some styling to Packages CSS

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import './index.css'
 import Layout from '../Layout/index'
 import Services from '../Services/index'
+import PageOne from '../Packages/index'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Layout>
+          <PageOne />
           <Services />
         </Layout>
       </div>
--- client/src/components/Packages/index.js
+++ client/src/components/Packages/index.js
@@ -3,9 +3,60 @@ import './index.css'
 
 class Packages extends Component {
   render() {
+    let pageOneStyle = {}
+    let formStyle = {}
+    let inputStyle = {}
+    let labelStyle = {}
+    let textAreaStyle = {}
     return (
       <div className="wrapper">
-        <h2>Welcome to React</h2>
+        <div className="page-one" style={pageOneStyle}>
+          <form style={formStyle}>
+            <h2>What is your occasion?</h2>
+            <label>
+              Social
+              <input style={inputStyle} type="checkbox" className="social" value="Social" />
+            </label>
+            <label>
+              Corporate
+              <input style={inputStyle} type="checkbox" className="corporate" value="Corporate" />
+            </label>
+
+            <textarea style={textAreaStyle} className="textarea" rows="10" cols="50">
+              Please explain your event type further (optional)
+            </textarea>
+
+            <h2>Guest Count</h2>
+            <input style={inputStyle} type="number" className="quantity" value="Guestcount" min="1" max="150" />
+
+            <h2>Do you have a venue?</h2>
+            <label>
+              Yes
+              <input style={inputStyle} type="checkbox" className="yes-venue" value="yes" />
+            </label>
+            <label>
+              No
+              <input style={inputStyle} type="checkbox" className="no-venue" value="no" />
+            </label>
+
+            <h3>Event Date</h3>
+            <input style={inputStyle} type="date" className="date" value="Calendar" />
+
+            <h3>Event Time</h3>
+            <label>
+              Morning
+              <input style={inputStyle} type="checkbox" className="time-morning" value="Morning" />
+            </label>
+            <label>
+              Afternoon
+              <input style={inputStyle} type="checkbox" className="time-afternoon" value="Afternoon" />
+            </label>
+            <label>
+              Evening
+              <input style={inputStyle} type="checkbox" className="time-evening" value="Evening" />
+            </label>
+          </form>
+        </div>
       </div>
     )
   }
