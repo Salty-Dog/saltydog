86a2398b1543ba0c121e4904d0109b345a8b50db
JSLinn727 <justinslinn727@gmail.com>
2017-08-27 14:52:32 -0400

    Fixed values in drop downs

--- client/src/components/Welcome/index.js
+++ client/src/components/Welcome/index.js
@@ -28,7 +28,7 @@ class Welcome extends Component {
           </div>
           <div className="count">
             <h3>Guest Count</h3>
-            <input type="number" value="count" min="1" max="150" />
+            <input type="number" min="1" max="150" />
           </div>
           <button onClick={this.changePage}>Go To Next Page</button>
         </div>
