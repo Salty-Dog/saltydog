5304ee95d4bdf3e3bd991887f37977e5f25a968b
John Sawyer <sawyer@sawyermerchant.com>
2017-08-27 09:26:29 -0400

    Remove js buildpack

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
-
   def fallback_index_html
-    puts "***********Hit Fallback Route*************"
-    render file: 'public/index.html'
+    render :file => 'public/index.html'
   end
 end
