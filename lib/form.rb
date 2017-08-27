c169c6ad237ba9b10108cb90156fc8dfc401b52a
Jose A. Campos <jjoseccampos@gmail.com>
2017-08-27 11:30:32 -0400

    capybara config of data inputting from API

--- lib/form.rb
+++ lib/form.rb
@@ -2,24 +2,28 @@ require 'capybara'
 require 'phantomjs'
 require 'capybara/poltergeist'
 
+def transmitter(data)
+
 session = Capybara::Session.new(:poltergeist)
 
 session.visit "http://saltblockhospitality.com/intake-form-plan/"
 
-session.fill_in('field44589333', with: 'foo@example.com')
+session.fill_in('field44589333', with: data.request.email)
 
-session.fill_in('field44589331-first', with: 'bob')
-session.fill_in('field44589331-last', with: 'jones')
-session.fill_in('field44589332', with: '(813) 555-1212')
-session.fill_in('field44589333', with: 'bob@example.com')
+session.fill_in('field44589331-first', with: data.request.name)
+session.fill_in('field44589331-last', with: data.request.lastname)
+session.fill_in('field44589332', with: data.request.phone
+session.fill_in('field44589333', with: data.request.email)
+
+time = data.request.time
 
 session.select('12', from: 'field52854717H')
 session.select('00', from: 'field52854717I')
 session.select('PM', from: 'field52854717A')
 
-session.fill_in('field44589335', with: 'a')
-session.fill_in('field44589337', with: '20')
-session.fill_in('field44589338', with: 'b')
+session.fill_in('field44589335', with: data.request.location)
+session.fill_in('field44589337', with: data.request.party_size)
+session.fill_in('field44589338', with: data.request.occasion)
 
 session.check('Bar Services')
 session.check('Event Design')
@@ -27,11 +31,11 @@ session.check('Rentals')
 
 session.execute_script("document.querySelector('.fsSubmitButton').click()")
 
+# # sleep 1
+# session.save_and_open_screenshot('response.png', full: true)
+#
 # if session.has_content?("Thank you")
 #   puts "Submitted"
 # else
 #   puts "Error"
 # end
-#
-# sleep 1
-# session.save_and_open_screenshot('response.png', full: true) 
