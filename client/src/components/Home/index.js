43ada80bdd9471ba76d5f6bb89430ea7f906a026
JSLinn727 <justinslinn727@gmail.com>
2017-08-27 13:17:32 -0400

    Added Home component

---
+++ client/src/components/Home/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './index.css'
+import Services from '../Services/'
+import Welcome from '../Welcome/'
+import Plan from '../Plan/'
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <Plan history={this.props.history} />
+        {/* <Services />
+        <Welcome /> */}
+      </div>
+    )
+  }
+}
+
+export default Home
